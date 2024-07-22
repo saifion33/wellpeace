9bc8bdca8b4598d5b7c31dac614d6b431ab8a03e
saifion33 <saifiazeem33@gmail.com>
2024-07-22 17:11:46 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import wellpeaceLogo from "./assets/wellpeace logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <img src={wellpeaceLogo} alt="wellpeace logo" />
+    </div>
+  );
+}
+
+export default App;
