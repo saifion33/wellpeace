bfcfe30c2f4515e8e88452a46bcd6cf89e0c5205
Delta Student <someone@email.com>
2024-07-24 21:47:57 +0530

    Added Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import wellpeaceLogo from "./assets/wellpeace logo.png";
+import Loginpage from "./Components/Loginpage.js";
 import "./App.css";
-
 function App() {
   return (
     <div>
-      <img src={wellpeaceLogo} alt="wellpeace logo" />
+      {/* <img src={wellpeaceLogo} alt="wellpeace logo" /> */}
+      <Loginpage />
     </div>
   );
 }
