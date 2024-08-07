d7335c1472716b5db8d7e22c76cbd9be32af25dd
saifion33 <saifiazeem33@gmail.com>
2024-08-07 17:20:26 +0530

    add community icon in bottom navigation bar

--- src/Components/common/BottomNavigation.tsx
+++ src/Components/common/BottomNavigation.tsx
@@ -2,15 +2,17 @@ import { FaRegUser, FaUser } from 'react-icons/fa'
 import { GoHome, GoHomeFill } from 'react-icons/go'
 import { IoBag, IoBagOutline,} from 'react-icons/io5'
 import { MdOutlinePhotoLibrary, MdPhotoLibrary } from 'react-icons/md'
+import { PiUsersThree, PiUsersThreeFill } from 'react-icons/pi'
 
 const BottomNavigation = () => {
    
   return (
     <footer className="flex justify-between mt-10 p-2 sticky bg-pinkishPurple bottom-0 left-0 w-full text-2xl ">
-        {location.pathname==='/'?<GoHomeFill className='text-[#f381c2]'/>:<GoHome className='text-stone-50'/>}
-        {location.pathname==='/products'?<IoBag className='text-[#f381c2]'/>:<IoBagOutline className='text-stone-50'/>}
-        {location.pathname==='/memory'?<MdPhotoLibrary className='text-[#f381c2]'/>:<MdOutlinePhotoLibrary className='text-stone-50'/>}
-        {location.pathname==='/user'?<FaUser className='text-[#f381c2]'/>:<FaRegUser className='text-stone-50'/>}
+        {location.pathname==='/'?<GoHomeFill className='text-[#f381c2] cursor-pointer'/>:<GoHome className='text-stone-50 cursor-pointer'/>}
+        {location.pathname==='/products'?<IoBag className='text-[#f381c2] cursor-pointer'/>:<IoBagOutline className='text-stone-50 cursor-pointer'/>}
+        {location.pathname==='/community'?<PiUsersThreeFill className='text-[#f381c2] cursor-pointer'/>:<PiUsersThree className='text-stone-50 cursor-pointer' />}
+        {location.pathname==='/memory'?<MdPhotoLibrary className='text-[#f381c2] cursor-pointer'/>:<MdOutlinePhotoLibrary className='text-stone-50 cursor-pointer'/>}
+        {location.pathname==='/user'?<FaUser className='text-[#f381c2] cursor-pointer'/>:<FaRegUser className='text-stone-50 cursor-pointer'/>}
     </footer>
   )
 }
